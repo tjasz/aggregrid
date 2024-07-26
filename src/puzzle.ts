7a02b75337dfab2d235041ccfe0e2af20aaf6830
tjasz <nukorausa@hotmail.com>
2024-07-26 12:37:06 -0700

    Add limited ability to solve puzzles

--- src/puzzle.ts
+++ src/puzzle.ts
@@ -1,4 +1,4 @@
-import { countingSequence } from "./algorithm";
+import { countingSequence, factorizations } from "./algorithm";
 import Grid from "./grid";
 
 export default class Puzzle {
@@ -31,6 +31,28 @@ export default class Puzzle {
       valueOptions.push([]);
       for (let j = 0; j < this.size; j++) {
         valueOptions[i].push(allowedValues.slice());
+        // limit value options for this cell by its row product
+        if (this.rowProducts[i] !== undefined) {
+          const rowFactorOptions = factorizations(this.rowProducts[i]!, this.size)
+            .filter(factorization =>
+              factorization.every(n => allowedValues.includes(n)) &&
+              (!this.uniqueValues || factorization.length === new Set(factorization).size) &&
+              factorization.some(n => valueOptions[i][j].includes(n))
+            )
+            .flat();
+          valueOptions[i][j] = rowFactorOptions;
+        }
+        // further limit value options for this cell by its column product
+        if (this.colProducts[j] !== undefined) {
+          const colFactorOptions = factorizations(this.colProducts[j]!, this.size)
+            .filter(factorization =>
+              factorization.every(n => allowedValues.includes(n)) &&
+              (!this.uniqueValues || factorization.length === new Set(factorization).size) &&
+              factorization.some(n => valueOptions[i][j].includes(n))
+            )
+            .flat();
+          valueOptions[i][j] = Array.from(new Set(valueOptions[i][j]).intersection(new Set(colFactorOptions)));
+        }
       }
     }
     console.log(valueOptions);
