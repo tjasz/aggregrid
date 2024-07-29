81b1fa7610ac47ad19e74c8c1d3fec4bf9b7a4ce
tjasz <nukorausa@hotmail.com>
2024-07-29 09:18:29 -0700

    Add 2x2 size option

--- src/NewGameForm.tsx
+++ src/NewGameForm.tsx
@@ -12,8 +12,9 @@ export function NewGameForm({ onNewGame }: NewGameFormProps) {
     <label htmlFor="uniqueValues">Unique Values</label>
     <input type="checkbox" name="uniqueValues" onChange={e => setUniqueValues(e.target.checked)} defaultChecked={uniqueValues} />
     <select onChange={e => setSize(parseInt(e.target.value))} defaultValue={size}>
-      <option value="3">3</option>
-      <option value="4">4</option>
+      <option value="2">2x2</option>
+      <option value="3">3x3</option>
+      <option value="4">4x4</option>
     </select>
     <br />
     <button onClick={() => onNewGame(size, uniqueValues)}>New Puzzle</button>
