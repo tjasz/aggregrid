7d95ae6292511104c68619ad53109a72fdb160cc
tjasz <nukorausa@hotmail.com>
2024-07-30 15:03:55 -0700

    Add details to header and footer

--- src/AppFooter.tsx
+++ src/AppFooter.tsx
@@ -1,5 +1,7 @@
+import packageJson from '../package.json'
+
 export default function AppFooter() {
   return <div className="app-footer">
-    <p>&copy; 2024 Tyler Jaszkowiak</p>
+    <p>&copy; 2024 Tyler Jaszkowiak, version {packageJson.version}</p>
   </div>
 }
--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -1,5 +1,7 @@
+import packageJson from '../package.json'
+
 export default function AppHeader() {
   return <div className="app-header">
-    <h1>Aggregrid</h1>
+    <h1><a href={packageJson.homepage}>{packageJson.name}</a></h1>
   </div>
 }
